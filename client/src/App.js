4e4e22c6ef702591aa73c18d1cf49dfddce056e7
William Lord <lordwilliam94@gmail.com>
2020-09-04 13:57:28 +1000

    Added bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route } from "react-router-dom";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
 import './App.css';
 
