450bd92154e3b2be307bb05b2ad14fe3684db6e0
misko <misko2da@gmail.com>
2020-09-05 11:39:28 +1000

    Auren pushing to own branch

--- client/src/components/Login.component.js
+++ client/src/components/Login.component.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 
-export default class CreateRegistration extends Component{
+export default class Login extends Component{
     
     render(){
         return(
             <div>
-                <h3>Test</h3>
+                <h3>Login
+                </h3>
             </div>
         )
     }
