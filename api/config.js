706952ea6ab8f4ede5040d39cf568ef9959036ab
Abdiaziz Farah <abdiazizfarah5@gmail.com>
2020-09-06 21:03:13 +1000

    added user reigstration server-side functionality

--- api/config.js
+++ api/config.js
@@ -3,8 +3,8 @@ const config = {};
 config.host = process.env.HOST || "https://portfoliodb.documents.azure.com:443/";
 config.authKey =
   process.env.AUTH_KEY || "Yub2LYLDNkJriGm6iNboj9m3EUvIrY8GmITuuANpLKOm0FFt41lHAQQmHI2ZnagMyAfQYzVq3tpX5GhFht2GmA==";
-config.databaseId = "ToDoList";
-config.containerId = "Items";
+config.databaseId = "techPiratesApplication";
+config.containerId = "Users";
 config.partitionKey = { kind: "Hash", paths: ["/category"] };
 if (config.host.includes("https://localhost:")) {
   console.log("Local environment detected");
