5b2679b59efd53f73e0fe5c83bd9a5d296b19f99
Abdiaziz Farah <abdiazizfarah5@gmail.com>
2020-09-11 20:12:04 +1000

    removed sensitive data

--- api/config/db.js
+++ api/config/db.js
@@ -2,8 +2,7 @@ require('dotenv').config()
 const mongoose = require("mongoose");
 const InitiateMongoServer = async () => {
   console.log(process.env.USERNAME)
-  //const MONGOURI = "mongodb+srv://"+process.env.USERNAME+":"+process.env.PASSWORD+"@cluster0.fai1l.azure.mongodb.net/"+process.env.DATABASE_NAME+"?retryWrites=true&w=majority";
-  const MONGOURI = "mongodb+srv://abdiaziz:Hello@123@cluster0.fai1l.azure.mongodb.net/eporfolio?retryWrites=true&w=majority";
+  const MONGOURI = "mongodb+srv://"+process.env.USERNAME+":"+process.env.PASSWORD+"@cluster0.fai1l.azure.mongodb.net/"+process.env.DATABASE_NAME+"?retryWrites=true&w=majority";
   try {
     await mongoose.connect(MONGOURI, {
       useNewUrlParser: true,
