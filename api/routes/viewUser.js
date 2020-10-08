37e6946cbab2635f6c356ff70ffad86e5cc6cd26
misko <misko2da@gmail.com>
2020-10-08 18:43:07 +1100

    fixed formatting

--- api/routes/viewUser.js
+++ api/routes/viewUser.js
@@ -2,9 +2,13 @@ const {db} = require('../utility/admin')
 
 exports.viewUser = (req, res) => {
     db.doc(`/users/${req.params.handle}/data/aboutme`).get().then(doc => {
-        return res.json(doc)
-    }).catch(err => {
-        console.error(err);
-        return res.status(500).json({ error: `something went wrong` });
-    });
-}
+        let aboutMe = {};
+        if(doc.exists){
+            aboutMe = doc.data().aboutMe;
+            return res.status(200).json({aboutMe});
+        }
+        }).catch(err => {
+            console.error(err);
+            return res.status(500).json({error:err.code})
+        })
+    }
