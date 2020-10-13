bd2ae440e3088a3ce3e6608161e31d8bcaff0d6f
William Lord <lordwilliam94@gmail.com>
2020-10-13 17:14:27 +1100

    Dropzone now adds files to already dropped-in files. Users can now remove files if they have already been dropped in.

--- client/src/components/profileComponents/uploadPortfolio.component.js
+++ client/src/components/profileComponents/uploadPortfolio.component.js
@@ -3,28 +3,55 @@ import Axios from 'axios'
 import FileUpload from "../fileUpload.component"
 
 import { Form, Button } from 'react-bootstrap'
-
+import {Card, CardHeader, CardMedia, CardContent, CardActions, Collapse, IconButton, Typography} from '@material-ui/core';
+import {Favorite, Share, ExpandMore, Edit, Delete, Remove, ZoomOutMap, Folder, PictureAsPdfOutlined, Image} from '@material-ui/icons';
+import {List, ListItem, ListItemAvatar, ListItemSecondaryAction, ListItemText, Avatar} from '@material-ui/core';
 
 function UploadPortfolio (){
     const [PortfolioName, setPortfolioName] = useState('');
     const [Description, setDescription] = useState('');
-    const [Files, setFiles] = useState([])
 
-    const updateFiles = (newFiles) =>{
-        setFiles(newFiles);
-        
+    // Files that have been accepted by the DND interface
+    const [AccceptedDNDFiles, setAccceptedDNDFiles] = useState([]);
+    const [RejectedDNDFiles, setRejectedDNDFiles] = useState([]);
+
+    // Files that will be uploaded to the database on submit
+    const [AcceptedFiles, setAcceptedFiles] = useState([]);
+
+    const updateAccepted = (acceptedFiles) => {
+        setAccceptedDNDFiles(acceptedFiles);
+        // Filter removes duplicate files by .name property; sorts by .name
+        let combineFiles = AcceptedFiles.concat(acceptedFiles).filter((file, index, self) =>
+            self.findIndex(f => f.name === file.name) === index
+        ).sort(
+            (f1, f2) => {
+                return f1.name < f2.name;
+            }
+        )
+        setAcceptedFiles(combineFiles);
     }
+
+    const updateRejected = (rejectedFiles) => {
+        setRejectedDNDFiles(rejectedFiles);
+    }
+
+    const displayAcceptedFiles = AcceptedFiles.map(file => (
+        <li key={file.path}>
+        {file.path} - {file.size} bytes
+        </li>
+    ));
+
 
     const onSubmit = (event) => {
         event.preventDefault();
         const variables = {
             name: PortfolioName,
             description: Description,
-            files: Files
+            files: AcceptedFiles
         }
         console.log("Number of Files on submission", variables.files.length)
 
-        Files.forEach((file) => {
+        AcceptedFiles.forEach((file) => {
             const reader = new FileReader()
             reader.onabort = () => console.log('file reading was aborted')
             reader.onerror = () => console.log('file reading has failed')
@@ -60,6 +87,17 @@ function UploadPortfolio (){
             })
             .catch( (error) => console.log(error))
     }
+
+    function getListItemIcon(fileType){
+        switch(fileType){
+            case "application/pdf":
+                return <PictureAsPdfOutlined />;
+            case "image/png" || "image/jpeg":
+                return <Image />
+            default:
+                return <Folder />;
+        }
+    }
     return(
         <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
             <Form id="uploadForm" onSubmit={onSubmit}>
@@ -79,7 +117,7 @@ function UploadPortfolio (){
                 </Form.Group>
                 
                 <div className="file-upload-container">
-                    <FileUpload refreshFunction={updateFiles}/>
+                    <FileUpload updateAccepted={updateAccepted} updateRejected={updateRejected}/>
                 </div>
                 <br/>
                 <Button
@@ -89,6 +127,32 @@ function UploadPortfolio (){
                     onSubmit={onSubmit}>
                     Submit
                 </Button>
+
+                <List>
+                    {AcceptedFiles.map((file, index) => 
+                    <ListItem key={index}>
+                        <ListItemAvatar>
+                        <Avatar>
+                            {getListItemIcon(file.type)}
+                        </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText
+                        primary={file.name}
+                        />
+                        <ListItemSecondaryAction>
+                        <IconButton edge="end" aria-label="delete"
+                            onClick={() => {
+                                const newFiles = [...AcceptedFiles]
+                                newFiles.splice(index, 1);
+                                setAcceptedFiles(newFiles)
+                            }}
+                        >
+                            <Delete />
+                        </IconButton>
+                        </ListItemSecondaryAction>
+                    </ListItem>,
+                    )}
+                </List>
                 
 
             </Form>
