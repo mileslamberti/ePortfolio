c7b4df22d740a78bd773941f0214219ce825435f
misko <misko2da@gmail.com>
2020-10-14 14:12:27 +1100

    returns the projectID of the saved project

--- api/routes/projects.js
+++ api/routes/projects.js
@@ -9,8 +9,8 @@ exports.saveProject = (req, res) => {
 
   const projectID=req.body.projectID;
   //db.collection(`/users/${req.user.handle}/data/projects/`).doc(projectID).set({aboutMe}).then(doc => {
-  db.doc(`/users/${req.user.handle}/projects/${projectID}`).set({project}).then(doc => {
-      return res.json({ message: `project added` })
+    db.doc(`/users/${req.user.handle}/projects/${projectID}`).set({project}).then(doc => {
+      return res.json({ message: `${projectID} added` })
   }).catch(err => {
       console.error(err);
       return res.status(500).json({ error: `something went wrong` });
