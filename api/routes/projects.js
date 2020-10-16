190b057394140593f4b392eed7161b863938b2c0
misko <misko2da@gmail.com>
2020-10-16 13:36:56 +1100

    Added projectID to projects as well as routes for getting projects

--- api/routes/projects.js
+++ api/routes/projects.js
@@ -3,6 +3,7 @@ const {db} = require('../utility/admin')
 
 exports.saveProject = (req, res) => {
   const project = {
+    projectID :req.body.projectID,
     title : req.body.title,
     description : req.body.description,
     files : req.body.files,
@@ -17,17 +18,66 @@ exports.saveProject = (req, res) => {
       return res.status(500).json({ error: `something went wrong` });
   });
 }
-exports.getProjects = (req, res) => {
-  return res.json({ message: `looking for projects??` })
+exports.getProjects = async (req, res) => {
+  var projects = [];
+  const databaseSnapshot = await db.collection(`/users/${req.user.handle}/projects`).get();
+  databaseSnapshot.forEach(project => {
+      projects.push(project.data().project);
+  });
+  return res.status(200).json({ projects });
 }
 
 exports.getProject = (req, res) => {
-  db.doc(`/users/${req.user.handle}/projects/${req.params.projectID}`).get().then(doc => {
+  const userHandle = req.user.handle;
+  if (!userHandle){
+    return res.status(206).json({ error: "no user given"});
+  }
+  db.doc(`/users/${userHandle}/projects/${req.params.projectID}`).get().then(doc => {
     if (doc.exists){
-      project=doc.data()
+      project=doc.data().project;
       return res.status(200).json({ project });
     } else {
       return res.status(206).json({error: `${req.params.projectID} does not exist under this user`})
     }
   })
+}
+exports.getAllProjectCards = async (req, res) => {
+  //TODO CHANGE to use UserAuth
+  //const userHandle = req.user.handle;
+  const userHandle = req.body.handle;
+  if (!userHandle){
+    return res.status(206).json({ error: "no user given"});
+  }
+  const snapshot = await db.collection(`/users/${userHandle}/projects/${req.params.projectID}/cards/`).get()
+    var cards=[];
+    snapshot.forEach(card => {
+      cards.push(card.data());
+    });
+    if (cards.length > 0){
+      return res.status(200).json({cards});
+    }
+    return res.status(206).json({ error: "no data in users collection"});
+}
+
+exports.addProjectCard = (req, res) => {
+    //TODO CHANGE to use UserAuth
+  const userHandle = req.body.handle;
+  if ( !hasCardData(req.body) ){
+    return res.status(400).json( { error: "req body does not contain all card details"})
+  }
+  const card = req.body;
+  db.doc(`/users/${userHandle}/projects/${newCard.projectID}/cards/${newCard.id}`).set({card})
+    .then( doc => {
+      return res.json({ message: `card ${doc.id} created` })
+    }).catch(err => {
+        console.error(err);
+        return res.status(500).json({ error: `something went wrong` });
+    });
+}
+
+const hasCardData = (card) => {
+  if ( !card.projectID || !card.id || !card.position || !card.title || !card.subtitle || !card.description || !card.img){
+    return false;
+  }
+  return true;
 }
