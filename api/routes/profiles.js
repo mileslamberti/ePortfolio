19f9ead5e2605f6fa97bda191cb85bfb5698dd68
misko <misko2da@gmail.com>
2020-10-24 09:35:42 +1100

    Only finds handles of users with public profiles

--- api/routes/profiles.js
+++ api/routes/profiles.js
@@ -4,7 +4,11 @@ exports.getAllUsers = async (req,res) => {
     var users = [];
     const databaseSnapshot = await db.collection(`/users`).get();
     databaseSnapshot.forEach(user => {
-        users.push(user.data().handle);
+        if(user.data().hasOwnProperty('private')){
+            if (!user.data().private){
+                users.push(user.data().handle);
+            }
+        }
     });
     return res.json({ users });
 }
