5733cebe6b68e1d1b9cac4636973ed50297def1b
Girish Mohan Madnani <gmadnani@student.unimelb.edu.au>
2020-11-10 01:39:09 +0400

    display tags and images

--- client/src/components/profilespage.component.js
+++ client/src/components/profilespage.component.js
@@ -12,7 +12,9 @@ const API_URL = "http://localhost:5000/eportfolio-4760f/us-central1/api";
 function ProfilesPage(){
 
     const [search, setSearch] = useState('');
-    const [profiles, setProfiles, setTags] = useState([]);
+    const [profiles, setProfiles] = useState([]);
+    const [Tags, setTags] = useState([]);
+    const [Dp, setDp] = useState([]);
 
     useEffect( () => {
         axios.get(API_URL + "/profiles")
@@ -20,6 +22,7 @@ function ProfilesPage(){
                 res.data.users.forEach(user => {
                     getProfileInfo(user);
                     getTagsInfo(user);
+                    getDpInfo(user);
                 })
             })
             .catch( err => {
@@ -43,23 +46,33 @@ function ProfilesPage(){
     //         </div>
     //     );
     // }
-    const saveProfileCard = (handle, aboutMe, tags) => {
+    const saveProfileCard = (handle, aboutMe) => {
         const newProfile = {
             image: '', 
             displayName: aboutMe.displayName, 
             handle: handle,
             description: aboutMe.description,
         }
-        console.log(newProfile);
+        //console.log(newProfile);
         setProfiles(profiles => profiles.concat([newProfile]));
     }
 
-    const saveTagsCard = (tags) => {
-        const newProfile = {
-           tags: tags
+    const saveTagsCard = (handle, tags) => {
+        const newtags = {
+            handle:handle,
+            tags: tags
         }
-        console.log(newProfile);
-        setTags(profiles => profiles.concat([newProfile]));
+        //console.log(newtags);
+        setTags(Tags => Tags.concat([newtags]));
+    }
+
+    const saveDpCard = (handle, dp) => {
+        const newDp = {
+            handle:handle,
+            dp: dp
+        }
+        console.log(newDp);
+        setDp(Dp => Dp.concat([newDp]));
     }
 
     const getProfileInfo = (handle) => {
@@ -79,28 +92,53 @@ function ProfilesPage(){
         axios.get(API_URL+`/${handle}/tags`).then( res => {
             switch (res.status){
                 case 200:
-                    saveTagsCard(res.data.tags);
+                    saveTagsCard(handle, res.data.tags);
                     break;
                 case 204:
-                    // user has no about me 
+                    // user has no tags 
                     break;
             } 
         }).catch( err => console.log(err))
     }
+
+    const getDpInfo = (handle) => {
+        axios.get(API_URL+`/${handle}/dp`).then( res => {
+            switch (res.status){
+                case 200:
+                    saveDpCard(handle, res.data.profilePic);
+                    break;
+                case 204:
+                    // user has no tags 
+                    break;
+            } 
+        }).catch( err => console.log(err))
+    }
+
     const filteredProfiles = profiles.filter ( profile => {
        return profile.displayName.includes( search )
     })
+
+    const displayTags = Tags.filter( tag =>{
+        return tag.tags
+    })
+
+    const displayDp = Dp.filter( dp =>{
+        return dp.dp
+    })
+    // const filteredTags = Tags.filter ( profile => {
+    //     return profile.tags.includes( search )
+    //  })
 
     const renderCard = (card, index) => {
         return(
             <div className="col-md-3" style={{ marginTop: "20px" }}>
                 <Card style={{ width: '18rem' }} key={ index }>
-                <Card.Img variant="top" src="holder.js/100px180" src= {card.image}/>
+                <Card.Img variant="top" src="holder.js/100px180" src= {card.dp}/>
                 <Card.Body>
-                <Card.Img variant="top" src="holder.js/100px180" src= {card.image}/>
+                {/* <Card.Img variant="top" src="holder.js/100px180" src= {card.image}/> */}
                     <Card.Title>{card.displayName}</Card.Title>
                     <Card.Text>{card.description}</Card.Text>
-                    <Card.Text>{card.tag}</Card.Text>
+                    <Card>{card.tags}</Card>
                     <Button href={`/${card.handle}`} variant="primary">View Profile</Button>
                 </Card.Body>
                 </Card>
@@ -110,11 +148,12 @@ function ProfilesPage(){
     }
 
     return (
-        
         <div >
             <input type="text" placeholder= "Search" onChange= { e => setSearch(e.target.value) }/>
             <div className="row">
                 {filteredProfiles.map(renderCard)}
+                {displayTags.map(renderCard)}
+                {displayDp.map(renderCard)}
             </div>
         </div>
     );
