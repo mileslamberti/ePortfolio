ee6742c07cfc45ed53eda36ce31e0fc760378424
misko <misko2da@gmail.com>
2020-11-15 12:13:14 +1100

    update to include shorter numbers

--- client/src/components/profileComponents/userInfo.component.js
+++ client/src/components/profileComponents/userInfo.component.js
@@ -103,7 +103,7 @@ const UserInfo = (props) => {
     }
     const inCorrectFormat = (form) => {
         const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        const phoneRegex = /^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$/
+        const phoneRegex = /^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$|[0-9]{6,12}/
         const number = form.number.replace(/\s/g,'');
         const isEmail = emailRegex.test(form.email);
         const isPhoneNumber = phoneRegex.test(number);
