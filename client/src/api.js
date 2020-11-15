7d1b3ce5484368bfff4c29e0e83f8fa62e64a1e9
Abdiaziz Farah <abdiazizfarah5@gmail.com>
2020-11-15 16:23:19 +1100

    changed url hosting config

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  //baseURL: "http://localhost:5000/eportfolio-4760f/us-central1/api"
-  baseURL: `https://us-central1-tech-pirates-portfolio-web-app.cloudfunctions.net/api`,
+  baseURL:
+    "http://localhost:5000/tech-pirates-portfolio-web-app/us-central1/api",
+  // baseURL: `https://us-central1-tech-pirates-portfolio-web-app.cloudfunctions.net/api`,
 });
